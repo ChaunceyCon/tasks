ff01765999ad667069182e40759af889d0f0337b
ChaunceyCon <chauncey.oconnell@maine.edu>
2025-10-19 07:25:26 -0400

    Implement ChangeType component to toggle question type

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,35 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question",
+    );
+
+    const toggleType = (): void => {
+        setQuestionType(
+            questionType === "short_answer_question" ?
+                "multiple_choice_question"
+            :   "short_answer_question",
+        );
+    };
+
+    return (
+        <div>
+            <Button onClick={toggleType}>Change Type</Button>
+            <span>
+                {questionType === "multiple_choice_question" ?
+                    "Multiple Choice"
+                :   "Short Answer"}
+            </span>
+        </div>
+    );
 }
+
+// Old code
+// import React, { useState } from "react";
+// import { Button } from "react-bootstrap";
+// import { QuestionType } from "../interfaces/question";
+
+// export function ChangeType(): React.JSX.Element {
+//     return <div>Change Type</div>;
+// }
