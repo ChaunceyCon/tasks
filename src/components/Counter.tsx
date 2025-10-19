8eca74eea0197ee51a171f23e44148a504475e61
ChaunceyCon <chauncey.oconnell@maine.edu>
2025-10-19 08:17:24 -0400

    Fixing small error with curly brackets. Also finished

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,8 +5,29 @@ export function Counter(): React.JSX.Element {
     const [value, setValue] = useState<number>(0);
     return (
         <span>
-            <Button onClick={() => setValue(1 + value)}>Add One</Button>
+            <Button
+                onClick={() => {
+                    setValue(1 + value);
+                }}
+            >
+                Add One
+            </Button>
             to {value}.
         </span>
     );
 }
+
+// Old code (simply adding braces)
+
+// import React, { useState } from "react";
+// import { Button } from "react-bootstrap";
+
+// export function Counter(): React.JSX.Element {
+//     const [value, setValue] = useState<number>(0);
+//     return (
+//         <span>
+//             <Button onClick={() => setValue(1 + value)}>Add One</Button>
+//             to {value}.
+//         </span>
+//     );
+// }
