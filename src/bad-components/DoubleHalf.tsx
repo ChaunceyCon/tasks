cb2cda68e9a09f1e3cd0fb75b7b551f619abb4bf
ChaunceyCon <chauncey.oconnell@maine.edu>
2025-11-02 20:52:39 -0500

    Fixing up DoubleHalf.tsx

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,30 +1,5 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
-
-function Doubler(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(2 * dhValue);
-            }}
-        >
-            Double
-        </Button>
-    );
-}
-
-function Halver(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(0.5 * dhValue);
-            }}
-        >
-            Halve
-        </Button>
-    );
-}
 
 export function DoubleHalf(): React.JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
@@ -53,6 +28,62 @@ export function DoubleHalf(): React.JSX.Element {
     );
 }
 
+// Saved code for debugging
+// import React, { useState } from "react";
+// import { Button } from "react-bootstrap";
+// import { dhValue, setDhValue } from "./DoubleHalfState";
+
+// function Doubler(): React.JSX.Element {
+//     return (
+//         <Button
+//             onClick={() => {
+//                 setDhValue(2 * dhValue);
+//             }}
+//         >
+//             Double
+//         </Button>
+//     );
+// }
+
+// function Halver(): React.JSX.Element {
+//     return (
+//         <Button
+//             onClick={() => {
+//                 setDhValue(0.5 * dhValue);
+//             }}
+//         >
+//             Halve
+//         </Button>
+//     );
+// }
+
+// export function DoubleHalf(): React.JSX.Element {
+//     const [dhValue, setDhValue] = useState<number>(10);
+
+//     return (
+//         <div>
+//             <h3>Double Half</h3>
+//             <div>
+//                 The current value is: <span>{dhValue}</span>
+//             </div>
+//             <Button
+//                 onClick={() => {
+//                     setDhValue(2 * dhValue);
+//                 }}
+//             >
+//                 Double
+//             </Button>
+//             <Button
+//                 onClick={() => {
+//                     setDhValue(0.5 * dhValue);
+//                 }}
+//             >
+//                 Halve
+//             </Button>
+//         </div>
+//     );
+// }
+// -----------------------------------------------------------------
 // Problem original code
 // export function DoubleHalf(): React.JSX.Element {
 //     return (
