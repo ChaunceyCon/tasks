045f44ce95f5d3eb7541d1ce540fc9c7445d788e
ChaunceyCon <chauncey.oconnell@maine.edu>
2025-11-02 19:10:14 -0500

    In process of fixing DoubleHalf: lift state, remove nested, immutability

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -27,14 +27,42 @@ function Halver(): React.JSX.Element {
 }
 
 export function DoubleHalf(): React.JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Button
+                onClick={() => {
+                    setDhValue(2 * dhValue);
+                }}
+            >
+                Double
+            </Button>
+            <Button
+                onClick={() => {
+                    setDhValue(0.5 * dhValue);
+                }}
+            >
+                Halve
+            </Button>
         </div>
     );
 }
+
+// Problem original code
+// export function DoubleHalf(): React.JSX.Element {
+//     return (
+//         <div>
+//             <h3>Double Half</h3>
+//             <div>
+//                 The current value is: <span>{dhValue}</span>
+//             </div>
+//             <Doubler></Doubler>
+//             <Halver></Halver>
+//         </div>
+//     );
+// }
