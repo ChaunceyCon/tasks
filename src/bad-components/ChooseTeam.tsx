7d35d807a043a855c1ba7e03706b93b621bb0310
ChaunceyCon <chauncey.oconnell@maine.edu>
2025-11-02 21:08:44 -0500

    Adding brackets

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -31,7 +31,12 @@ export function ChooseTeam(): React.JSX.Element {
                     {PEOPLE.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={() => addMember(option)} size="sm">
+                            <Button
+                                onClick={() => {
+                                    addMember(option);
+                                }}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
